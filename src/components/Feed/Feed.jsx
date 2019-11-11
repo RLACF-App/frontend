335b6d7cb933e7049c0c0d7b50465a65526e3405
josh182014 <josh182014@gmail.com>
2019-11-11 15:33:52 -0700

    got rid of dummy data

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -3,7 +3,6 @@ import Opportunities from '../Opportunities/Opportunities';
 
 const Feed = ({ routeProps, opp }) => (
   <div className="feedContainer">
-    Welcome to your volunteer feed.
     <Opportunities routeProps={routeProps} opp={opp} />
   </div>
 );
