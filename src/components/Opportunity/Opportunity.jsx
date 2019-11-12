94803b65e242976701c5d9406152d5fee567a63e
josh182014 <josh182014@gmail.com>
2019-11-12 09:46:10 -0700

    cut off description if too long in feed

--- src/components/Opportunity/Opportunity.jsx
+++ src/components/Opportunity/Opportunity.jsx
@@ -1,25 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { DVOContext } from '../../contexts/DVOContext';
 import './opportunity.scss';
 
 const Opportunity = ({ routeProps, opp }) => {
-  const setOpp = useContext(DVOContext).setState;
+  // const setOpp = useContext(DVOContext).setState;
+
+  const [oppState, setOppState] = useState(opp);
+
+  useEffect(() => {
+    if (oppState.description.length > 259) {
+      setOppState({ ...oppState, description: `${opp.description.substring(0, 260)}...` });
+    }
+  }, []);
 
   const handleClick = () => {
     routeProps.history.push(`/opportunity/${opp.id}`);
-    setOpp((prevState) => ({
-      ...prevState,
-      selectedOpportunity: opp,
-    }));
+    // setOpp((prevState) => ({
+    //   ...prevState,
+    //   selectedOpportunity: opp,
+    // }));
   };
 
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
       routeProps.history.push(`/opportunity/${opp.id}`);
-      setOpp((prevState) => ({
-        ...prevState,
-        selectedOpportunity: opp,
-      }));
+      // setOpp((prevState) => ({
+      //   ...prevState,
+      //   selectedOpportunity: opp,
+      // }));
     }
   };
   return (
@@ -28,8 +36,8 @@ const Opportunity = ({ routeProps, opp }) => {
         <div className="image-container">
           <img src="https://images.unsplash.com/photo-1557660559-42497f78035b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2230&q=80" />
         </div>
-        <div className="opportunity-name"><strong>{opp.name}</strong></div>
-        <p className="opportunity-description">{opp.description}</p>
+        <div className="opportunity-name"><strong>{oppState.name}</strong></div>
+        <p className="opportunity-description">{oppState.description}</p>
         <div className="fade" />
       </div>
       <div className="share"><a href="#">Share</a></div>
